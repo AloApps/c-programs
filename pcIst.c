bd01f4e465dd5b486e89bc8b1fe13e58e92865ce
Akram Khan <alorweb@gmail.com>
2017-01-21 11:06:40 +0600

    Update pcIst.c

--- pcIst.c
+++ pcIst.c
@@ -1,2 +1,92 @@
 while(!succeed)
     try();
+
+
+
+
+s.replace(/hate/g, "love");
+
+
+if(youWant(){
+    youCan();
+}else{
+    youCant();
+}
+   
+   
+
+   
+   
+   
+// life motto
+if(sad()==true){
+    sad.stop();
+    beAwesome();
+}
+   
+   
+   
+   
+   
+<life> coding </life>
+
+
+
+
+while(alive)
+{
+    eat();
+    sleep();
+    code();
+}
+   
+   
+   
+   
+   
+   
+bool dead = false;
+void goDeeper()
+{
+    if(dead == true)
+        return;
+    goDeeper();
+}
+   
+int main()
+{
+    goDeeper();
+}
+   
+   
+   
+   
+   
+   
+   
+   
+while(noSuccess)
+{
+    tryAgain();
+    if(Dead)
+        break;
+}
+   
+   
+   
+   
+   
+   
+   
+   
+if(signature == autograph)
+{
+    moveOn();
+}
+else
+{
+    improveYourself();
+    betterThanBefore++;
+}
+   
+   
